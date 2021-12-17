259ec9d207bfdb6039e7bcc593992ca3613bad53
Z1A1 <33432323+Z1A1@users.noreply.github.com>
2021-12-17 07:39:01 +0530

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,41 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
+from typing import Any, Text, Dict, List
 #
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from rasa_sdk import Action, Tracker,FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+#
+# class ValidateNameForm(FormValidationAction):
+class ValidateName(FormValidationAction):
 
+    def name(self) -> Text:
 
-# This is a simple example for a custom action which utters "Hello World!"
+        return "validate_school_form"
 
- #from typing import Any, Text, Dict, List
-#
- #from rasa_sdk import Action, Tracker
- #from rasa_sdk.executor import CollectingDispatcher
-#
-#
- #class ActionHelloWorld(Action):
-#
-    # def name(self) -> Text:
-        # return "action_hello_world"
-#
-    # def run(self, dispatcher: CollectingDispatcher,
-             #tracker: Tracker,
-             #domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-         #dispatcher.utter_message(text="Hello World!")
-#
-        # return []
+    def validate_first_name_last_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `first_name` value."""
+        print(f"First name given = {slot_value} length = {len(slot_value)}")
+        if len(slot_value) <= 2:
+            dispatcher.utter_message(text=f"That's a very short name. I'm assuming you mis-spelled.")
+            return {"first_name_last_name": None}
+        else:
+            return {"first_name_last_name": slot_value}
+    def validate_acedamicyear(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+       if slot_value == "2021":
+           
+           return{"acedamicyear": slot_value}
+
+       else:
+           dispatcher.utter_message(text="Please select current year")
+           return {"acedamicyear": None}    
